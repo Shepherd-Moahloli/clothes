10d11a24e21635a617532ff4286119488cacf27a
unknown <shepherdmoahloli122@gmail.com>
2023-11-07 21:40:43 +0100

    just fixed the search button working well

--- my-project/js/main.js
+++ my-project/js/main.js
@@ -14,6 +14,35 @@ document
 
 document.getElementById("search").addEventListener("click", function () {
   var searchForm = document.getElementById("searchForm");
-  searchForm.style.display = "flex";
-  searchForm.style.width = "100vw";
+  var searchLi = document.getElementById("search");
+
+  if (searchForm.style.display === "flex") {
+    searchForm.style.display = "none";
+    searchLi.classList.remove("active");
+  } else {
+    searchForm.style.display = "flex";
+    searchForm.style.width = "100vw";
+    searchForm.style.margin = "0";
+    searchLi.classList.add("active");
+  }
+});
+
+document.getElementById("search").addEventListener("click", function () {
+  var searchBar = document.getElementById("searchBar");
+  var searchLi = document.getElementById("search");
+
+  if (searchBar.style.display === "none") {
+    searchBar.style.display = "flex";
+    searchLi.classList.add("active");
+  } else {
+    searchBar.style.display = "none";
+    searchLi.classList.remove("active");
+  }
+});
+
+document.getElementById("buttonId").addEventListener("click", function () {
+  var searchBar = document.getElementById("searchBar");
+  var searchLi = document.getElementById("search");
+  searchBar.style.display = "none";
+  searchLi.classList.remove("active");
 });
